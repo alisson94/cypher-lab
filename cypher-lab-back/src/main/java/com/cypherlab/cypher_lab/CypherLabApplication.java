9e8ac1e32def2e0b813310122b56274baa20ed58
Álisson Santana <45105953+alisson94@users.noreply.github.com>
2025-09-25 22:56:10 -0300

    initial back

---
+++ cypher-lab-back/src/main/java/com/cypherlab/cypher_lab/CypherLabApplication.java
@@ -0,0 +1,13 @@
+package com.cypherlab.cypher_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CypherLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CypherLabApplication.class, args);
+	}
+
+}
---
+++ cypher-lab-back/src/test/java/com/cypherlab/cypher_lab/CypherLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cypherlab.cypher_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CypherLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
