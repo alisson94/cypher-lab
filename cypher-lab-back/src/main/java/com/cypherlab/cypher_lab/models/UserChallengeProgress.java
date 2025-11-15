41d3307c5ca93437a20b4092e6b6395f6071ac99
Álisson Santana <45105953+alisson94@users.noreply.github.com>
2025-11-15 14:22:03 -0300

    Create UserChallengeProgress.java

---
+++ cypher-lab-back/src/main/java/com/cypherlab/cypher_lab/models/UserChallengeProgress.java
@@ -0,0 +1,131 @@
+package com.cypherlab.cypher_lab.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user_challenge_progress")
+public class UserChallengeProgress {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Usuario usuario;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "challenge_id", nullable = false)
+    private Challenge challenge;
+
+    @Column(name ="solved", nullable = false)
+    private Boolean solved =false;
+
+    @Column(name = "attempts", nullable = false)
+    private Integer attempts = 0;
+
+    @Column(name = "points_earned")
+    private Integer pointsEarned;
+
+    @Column(name = "solved_at")
+    private LocalDateTime solvedAt;
+
+    @Column(name = "first_attempt_at")
+    private LocalDateTime firstAttemptAt;
+
+    @Column(name = "last_attempt_at")
+    private LocalDateTime lastAttemptAt;
+
+    public UserChallengeProgress() {}
+
+    public UserChallengeProgress(Usuario usuario, Challenge challenge) {
+        this.usuario = usuario;
+        this.challenge = challenge;
+        this.firstAttemptAt = LocalDateTime.now();
+        this.lastAttemptAt = LocalDateTime.now();
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Challenge getChallenge() {
+        return challenge;
+    }
+
+    public void setChallenge(Challenge challenge) {
+        this.challenge = challenge;
+    }
+
+     public Boolean getSolved() {
+        return solved;
+    }
+
+    public void setSolved(Boolean solved) {
+        this.solved = solved;
+    }
+
+    public Integer getAttempts() {
+        return attempts;
+    }
+
+    public void setAttempts(Integer attempts) {
+        this.attempts = attempts;
+    }
+
+    public Integer getPointsEarned() {
+        return pointsEarned;
+    }
+
+    public void setPointsEarned(Integer pointsEarned) {
+        this.pointsEarned = pointsEarned;
+    }
+
+    public LocalDateTime getSolvedAt() {
+        return solvedAt;
+    }
+
+    public void setSolvedAt(LocalDateTime solvedAt) {
+        this.solvedAt = solvedAt;
+    }
+
+    public LocalDateTime getFirstAttemptAt() {
+        return firstAttemptAt;
+    }
+
+    public void setFirstAttemptAt(LocalDateTime firstAttemptAt) {
+        this.firstAttemptAt = firstAttemptAt;
+    }
+
+    public LocalDateTime getLastAttemptAt() {
+        return lastAttemptAt;
+    }
+
+    public void setLastAttemptAt(LocalDateTime lastAttemptAt) {
+        this.lastAttemptAt = lastAttemptAt;
+    }
+}
